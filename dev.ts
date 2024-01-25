e040ae3b4eb896e2825683510750ea2dc7db467d
Chris Knight <cknight1234@gmail.com>
2024-01-25 17:21:23 +0000

    fix: clean up debug logging and remove temporary workaround for preact issue which is now fixed

--- dev.ts
+++ dev.ts
@@ -5,23 +5,11 @@ import config from "./fresh.config.ts";
 import "$std/dotenv/load.ts";
 import { env } from "./consts.ts";
 
-// Disable debug logging
 const log_level = Deno.env.get(env.LOG_LEVEL);
-if (false && log_level !== "DEBUG") {
+
+if (log_level !== "DEBUG") {
+  // Disable debug logging
   console.debug = () => {};
 }
-
-//FIXME - temporarily disable console.error for preact logging bug
-const origConsoleError = console.error;
-console.error = (msg) => {
-  if (typeof msg === "string" && msg.includes("Improper nesting of table")) return;
-  origConsoleError(msg);
-};
-
-const origConsoleWarn = console.warn;
-console.warn = (msg) => {
-  if (typeof msg === "string" && msg.includes("plugin-transform-react-jsx-source")) return;
-  origConsoleWarn(msg);
-};
 
 await dev(import.meta.url, "./main.ts", config);
