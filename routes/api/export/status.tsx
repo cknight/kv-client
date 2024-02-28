11064d0573d83059714194d56d1d9dfddb938b12
Chris Knight <cknight1234@gmail.com>
2024-02-28 22:01:50 +0000

    chore: rename tests with extension tsx to ts

--- routes/api/export/status.tsx
+++ routes/api/export/status.tsx
@@ -1,7 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import { getExportStatus } from "../../../utils/state/state.ts";
 
-//query KV and respond with status:  keys retrieved, bytes retrieved.  For connections with size, estimate percentage complete
 export const handler: Handlers = {
   GET(req, ctx) {
     const exportId = new URL(req.url).searchParams.get("exportId");
