c6a74e7fe5fe52d92ec9475f4a5677cdb4c10699
Chris Knight <cknight1234@gmail.com>
2024-03-11 09:17:06 +0000

    chore: remove unused code

--- routes/import.tsx
+++ routes/import.tsx
@@ -11,43 +11,6 @@ interface ImportProps {
   error?: string;
 }
 
-export const handler: Handlers = {
-  async POST(req, ctx) {
-    const form = await req.formData();
-    const inputFileFormEntry: FormDataEntryValue | null = form.get("importFile");
-    const connectionId = new URL(req.url).searchParams.get("connectionId") || "";
-    const session = ctx.state.session as string;
-    if (inputFileFormEntry && inputFileFormEntry instanceof File) {
-      logDebug({ sessionId: session }, "  writing import file to disk");
-      const tempDir = await Deno.makeTempDir();
-      const fullPathFile = join(tempDir, inputFileFormEntry.name);
-      try {
-        // Step 1: Take the uploaded file and write it to disk
-        await Deno.writeFile(fullPathFile, new Uint8Array(await inputFileFormEntry.arrayBuffer()));
-        logDebug({ sessionId: session }, "  import file successfully written to disk");
-
-        // Step 2: Open KV connection to file and validate
-        const importFromKv = await Deno.openKv(fullPathFile);
-        await importFromKv.get(["a random key to test the connection"]);
-
-        // Step 3: Read all entries and write to KV
-        logDebug({ sessionId: session }, "  KV connection opened and validated");
-        const entries = await Array.fromAsync(importFromKv.list({ prefix: [] }));
-        const state = getUserState(session);
-        await setAll(entries, state.kv!, connectionId);
-        logDebug({ sessionId: session }, "  all entries written to KV");
-      } catch (e) {
-        logError({ sessionId: session }, "Failed to import", e);
-        return ctx.render({ error: e.message });
-      } finally {
-        // Step 4: Delete file from disk
-        await Deno.remove(tempDir, { recursive: true });
-      }
-    }
-    return await ctx.render();
-  },
-};
-
 export default async function Import(req: Request, props: RouteContext<ImportProps>) {
   const connections = await getConnections(props.state.session as string);
   const connectionId = new URL(req.url).searchParams.get("connectionId") || "";
