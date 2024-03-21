6ba9e7a99a9dfb73e7df7d267cb30ac37bd71ac7
Chris Knight <cknight1234@gmail.com>
2024-03-21 20:38:42 +0000

    test: display

---
+++ utils/ui/display_test.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "$std/assert/assert_equals.ts";
+import { asPercentString } from "./display.ts";
+
+Deno.test("Display", () => {
+  assertEquals(asPercentString(0.123456), "12%");
+  assertEquals(asPercentString(1), "100%");
+  assertEquals(asPercentString(0), "0%");
+  assertEquals(asPercentString(0.01), "1%");
+  assertEquals(asPercentString(0.01999999), "2%");
+});
